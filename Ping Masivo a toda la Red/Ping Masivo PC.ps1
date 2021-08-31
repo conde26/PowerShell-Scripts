79b4b74043947638a58d2b6131b91acd03a633ba
Conde <79221088+conde26@users.noreply.github.com>
2021-08-31 19:37:51 +0000

    Add files via upload

---
+++ Ping Masivo a toda la Red/Ping Masivo PC.ps1
@@ -0,0 +1,18 @@
+﻿#Script para comprobar equipos disponibles
+#Autor: Jose Conde
+
+ForEach ($ip in 1..254) 
+{
+    $computer = "192.168.1.$ip"
+    $status = Test-Connection $computer -count 1 -Quiet 
+    
+    if ($status -eq "True")
+    {
+        Write-Host "[!] Equipo: $computer" -ForegroundColor Yellow -NoNewline; Write-Host " - Encendido"  -ForegroundColor Green
+    }
+    else
+    {
+        Write-Host "[!] Equipo: $computer" -ForegroundColor Yellow -NoNewline; Write-Host  " - Apagado"  -ForegroundColor Red
+
+    }
+}
