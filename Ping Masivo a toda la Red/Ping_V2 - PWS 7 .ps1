134f054342ae4a9f01508fd98c4e79735759ef13
Conde <79221088+conde26@users.noreply.github.com>
2021-11-30 21:11:41 +0100

    Add files via upload

---
+++ Ping Masivo a toda la Red/Ping_V2 - PWS 7 .ps1
@@ -0,0 +1,13 @@
+﻿#Script para comprobar equipos disponibles en PWS 7
+#Autor: Jose Conde
+
+#Ping Multihilo - V2
+1..254 | ForEach-Object -Parallel {
+    $computer = "192.168.0.$_"
+    $status = Test-Connection $computer -count 1 -Quiet 
+    
+    if ($status -eq "True")
+    {
+        Write-Host "[!] Equipo: $computer" -ForegroundColor Yellow -NoNewline; Write-Host " - Encendido"  -ForegroundColor Green
+    }
+} -ThrottleLimit 100
