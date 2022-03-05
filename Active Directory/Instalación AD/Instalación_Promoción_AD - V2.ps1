6e9ded34b9b1570ed609d3755609ba90ba71c709
Conde <79221088+conde26@users.noreply.github.com>
2022-03-05 16:21:05 +0100

    Add files via upload

---
+++ Active Directory/Instalación AD/Instalación_Promoción_AD - V2.ps1
@@ -0,0 +1,28 @@
+﻿#Scripts Instalación AD 
+#Author: Jose Conde 
+
+#Propiedades en hastable
+$Propiedades = @{
+    domain = "dominio.com"
+    netbios = "dominio"
+	password = "strong&P4$$W0RD"
+}
+
+#Instalamos rol Active Directory
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+
+#Instalamos nuevo bosque y dominio 
+Install-ADDSForest -DomainMode "Win2012" `
+				   -ForestMode "Win2012" `
+				   -DomainName "$($Propiedades.domain)" `
+				   -DomainNetbiosName "$($Propiedades.netbios)" `
+				   -SafeModeAdministratorPassword (ConvertTo-SecureString -string "$($Propiedades.password)" -AsPlainText -Force) `
+				   -InstallDns:$true -CreateDnsDelegation:$false `
+				   -DatabasePath "C:\Windows\NTDS" `
+				   -LogPath "C:\Windows\NTDS" `
+				   -SysvolPath "C:\Windows\SYSVOL" `
+				   -NoRebootOnCompletion:$false `
+				   -Force:$true `
+				   
+#Reinciamos equipo, para aplicar cambios		  
+restart-computer
