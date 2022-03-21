f74336d8e1992dfb19a559d2a5909c51fab7b036
Conde <79221088+conde26@users.noreply.github.com>
2022-03-21 18:47:55 +0100

    Update Ping_V1 - PWS 5.ps1

--- Ping Masivo a toda la Red/Ping_V1 - PWS 5.ps1
+++ Ping Masivo a toda la Red/Ping_V1 - PWS 5.ps1
@@ -1,18 +1,10 @@
-#Script para comprobar equipos disponibles
+#Script para comprobar equipos disponibles en la red
 #Autor: Jose Conde
 
-ForEach ($ip in 1..254) 
-{
-    $computer = "192.168.1.$ip"
-    $status = Test-Connection $computer -count 1 -Quiet 
-    
-    if ($status -eq "True")
-    {
-        Write-Host "[!] Equipo: $computer" -ForegroundColor Yellow -NoNewline; Write-Host " - Encendido"  -ForegroundColor Green
-    }
-    else
-    {
-        Write-Host "[!] Equipo: $computer" -ForegroundColor Yellow -NoNewline; Write-Host  " - Apagado"  -ForegroundColor Red
+1..254 | % {
+        $status = Test-Connection 192.168.10.$_ -Count 1 -Quiet
 
-    }
+        if ($status){
+                  Write-Host "[!] Equipo: 192.168.10.$_" -ForegroundColor Yellow -NoNewline; Write-Host " - Encendido"  -ForegroundColor Green
+        }
 }
