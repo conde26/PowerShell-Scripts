e3b9967af848cd0dd1fa42052fa15a186d55c3d0
Conde <79221088+conde26@users.noreply.github.com>
2022-05-23 14:04:12 +0200

    Update GPO - Pagina_Principal_Cliente.ps1

--- Active Directory/GPOs/GPO - Pagina_Principal_Cliente.ps1
+++ Active Directory/GPOs/GPO - Pagina_Principal_Cliente.ps1
@@ -1,17 +1,20 @@
-﻿#Configurar página de inicio clientes
+#Configurar página de inicio clientes
 #Author: Jose Conde
 
+#Input domain
+Write-Host -NoNewline -ForegroundColor Yellow "[!] Indica el nombre de tu dominio: "; $dom=Read-Host 
+
 #Creamos GPO con un nombre y comentario
-New-GPO -Name "Pagina_Inicio" -Comment "Pagina de inicio la de nuestro instituto"
+New-GPO -Name "Pagina_Inicio" -Comment "Página por defecto en el navegador"
 
 #Añadimos un nombre y la clave del reistro 
-Set-GPRegistryValue -Name "Pagina_Inicio" -Key "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -ValueName Default_Page_URL -Type String -Value http://www.edu.xunta.gal/centros/iesmurallaromana/
-Set-GPRegistryValue -Name "Pagina_Inicio" -Key "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -ValueName "Search Page" -Type String -Value http://www.edu.xunta.gal/centros/iesmurallaromana/
-Set-GPRegistryValue -Name "Pagina_Inicio" -Key "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -ValueName "Start Page" -Type String -Value http://www.edu.xunta.gal/centros/iesmurallaromana/
-Set-GPRegistryValue -Name "Pagina_Inicio" -Key "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -ValueName "First Home Page" -Type String -Value http://www.edu.xunta.gal/centros/iesmurallaromana/
+Set-GPRegistryValue -Name "Pagina_Inicio" -Key "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -ValueName Default_Page_URL -Type String -Value https://rinconh4ck.blogspot.com/
+Set-GPRegistryValue -Name "Pagina_Inicio" -Key "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -ValueName "Search Page" -Type String -Value https://rinconh4ck.blogspot.com/
+Set-GPRegistryValue -Name "Pagina_Inicio" -Key "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -ValueName "Start Page" -Type String -Value https://rinconh4ck.blogspot.com/
+Set-GPRegistryValue -Name "Pagina_Inicio" -Key "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" -ValueName "First Home Page" -Type String -Value https://rinconh4ck.blogspot.com/
 
 #Añadimos la directiva
-New-GPLink -Name "Pagina_Inicio" -Target "DC=Dominio,DC=com"
+New-GPLink -Name "Pagina_Inicio" -Target "DC=$($dom.Split(".")[0]),DC=$($dom.Split(".")[1])"
 
 #Forzamos actualización 
 gpupdate /force 
