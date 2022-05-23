96224634414f7d257552b89f724a82463c96cf77
Conde <79221088+conde26@users.noreply.github.com>
2022-05-23 13:20:39 +0200

    Create User_Bulk - V2.ps1

---
+++ Active Directory/Usuarios en Bulk/Versión 4/User_Bulk - V2.ps1
@@ -0,0 +1,34 @@
+#Add users in bulk 
+#Author: Conde 
+
+param(
+    $file
+)
+
+
+#Get content from file
+Import-Csv -Delimiter ","  $file |  % {
+
+#Add user with properties    
+New-ADUser $($_.name) -AccountPassword $(ConvertTo-SecureString $($_.password) -AsPlainText -Force) `
+                 -HomeDirectory \\$(hostname)\Carpetas_Personales\$($_.name) `
+                 -HomeDrive Z: `
+                 -ProfilePath \\$(hostname)\Perfiles_Moviles\$($_.name) `
+                 -PasswordNeverExpires $true `
+                 -Enabled $true `
+                 -ScriptPath inicio.bat `
+
+#Add Unix Atributes
+Set-ADUser $($_.name) -Add @{uidnumber=$($_.uid); 
+                             gidnumber=$($_.guid); 
+                             unixHomeDirectory=$($_.home)
+                            }
+
+#Print verify message
+Write-host -ForegroundColor yellow -NoNewline "`n[!] Added User: "; Write-host -ForegroundColor Green "$($_.name.ToUpper())`n"
+Write-Host -ForegroundColor Yellow -NoNewline "`t[*] Name: "; Write-host -ForegroundColor Green "$($_.name)"
+Write-Host -ForegroundColor Yellow -NoNewline "`t[*] Password: " ; Write-host -ForegroundColor Green "$($_.password)"
+Write-Host -ForegroundColor Yellow -NoNewline "`t[*] UID: " ; Write-host -ForegroundColor Green "$($_.uid)"
+Write-Host -ForegroundColor Yellow -NoNewline "`t[*] GUID: " ; Write-host -ForegroundColor Green "$($_.guid)"
+Write-Host -ForegroundColor Yellow -NoNewline "`t[*] UnixHome: "; Write-host -ForegroundColor Green "$($_.home)"
+}
