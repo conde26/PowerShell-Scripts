28e44585ce81436c3798a20e5ae5abaf66626a1e
Conde <79221088+conde26@users.noreply.github.com>
2022-05-23 17:57:11 +0200

    Add files via upload

---
+++ Contraseñas de Wi-Fi/Get Wi-Fi Passwords.ps1
@@ -0,0 +1,17 @@
+﻿#Get Wi-Fi Password from Windows
+#Author: Conde
+
+#Get SSID
+$wireless_profiles=((netsh wlan show profiles) -match '\s{2,}:\s') -replace '.*:\s' , ''
+
+#Getting info
+$wireless_profiles | % {
+    #Get Password
+	netsh wlan show profile name=$_ key=clear | Select-String “clave\W+\:(.+)$” | % {
+        $password=$_.Matches.Groups[1].Value.Trim()
+            
+    }
+	#Get Name and password
+    Write-host -NoNewline -ForegroundColor Green "`n[*] $_ :"; Write-Host -ForegroundColor red " $password`n" 
+}
+
