7c78180f82055660f5581a8b848c98a88c505226
Conde <79221088+conde26@users.noreply.github.com>
2022-05-23 16:49:14 +0200

    Update Clave_Registro_GPO.ps1

--- Active Directory/GPOs/Clave registro/Clave_Registro_GPO.ps1
+++ Active Directory/GPOs/Clave registro/Clave_Registro_GPO.ps1
@@ -1,20 +1,26 @@
 #Script Para Ver Claves del Registro de GPOs
-#Author: Jose Conde
+#Author: Conde
 
-## Buscamos el GUID de la GPO (Sesión es una GPO que cree yo, en tu caso debes indicar la tuya)
-$gpoGuid = (Get-GPO -Name 'sesión').Id.ToString()
+Write-Host -ForegroundColor Yellow -NoNewline "[*] Full Domain Name: "; $domainName=Read-Host
+Write-Host -ForegroundColor Yellow -NoNewline "`n[*] GPO Name: "; $gpo=Read-Host
 
-## Buscamos el fichero registry.pol 
-$domainController = 'HOSTNAME_SERVER'
-$domainName = 'DOMAIN.COM'
-$registryPolPath = "\\$domainController\sysvol\$domainName\Policies\{$gpoGuid}\User"
-Get-ChildItem -Path $registryPolPath
-
-$regPolPath = Join-Path -Path $registryPolPath -ChildPath 'registry.pol'
-
-## Instalamos el modulo registry.pol 
+## Instalamos el modulo necesario
 Install-Module -Name GPRegistryPolicy
 
-## Vemos la clave y los valores de la GPO
-Parse-PolFile -Path $regPolPath
-pause
+## Buscamos el GUID de la GPO
+$gpoGuid = (Get-GPO -Name "$gpo").Id.ToString()
+
+## Buscamos el fichero registry.pol 
+$registryPolPath = "\\$(hostname)\sysvol\$domainName\Policies\{$gpoGuid}\Machine"
+$regPolPath = Join-Path -Path $registryPolPath -ChildPath 'registry.pol'
+
+#Comprobar si es clave de maquina o usuario
+if ( Test-Path -Path $regPolPath ) {
+    Clear-Host; Write-Host -ForegroundColor Yellow "`n[*] Getting KeyName and Values" 
+    Parse-PolFile -Path $regPolPath
+} else {
+    $registryPolPath = "\\$(hostname)\sysvol\$domainName\Policies\{$gpoGuid}\User"
+    $regPolPath = Join-Path -Path $registryPolPath -ChildPath 'registry.pol'
+    Clear-Host; Write-Host -ForegroundColor Yellow "`n[*] Getting KeyName and Values" 
+    Parse-PolFile -Path $regPolPath
+}
