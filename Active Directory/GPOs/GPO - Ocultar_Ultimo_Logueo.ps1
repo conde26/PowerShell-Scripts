7964bb8c64b45a203b579fb4da6fd1ff03f40898
Conde <79221088+conde26@users.noreply.github.com>
2022-05-23 14:02:32 +0200

    Update GPO - Ocultar_Ultimo_Logueo.ps1

--- Active Directory/GPOs/GPO - Ocultar_Ultimo_Logueo.ps1
+++ Active Directory/GPOs/GPO - Ocultar_Ultimo_Logueo.ps1
@@ -1,6 +1,9 @@
 ﻿#Ocultar inicios de sesión
 #Author: Jose Conde
 
+#Input domain
+Write-Host -NoNewline -ForegroundColor Yellow "[!] Indica el nombre de tu dominio: "; $dom=Read-Host 
+
 #Creamos GPO con un nombre y comentario
 New-GPO -Name "Ocultar Inicio" -Comment "ocultar el inicio de sesion de los usuarios"
 
@@ -8,7 +11,7 @@ New-GPO -Name "Ocultar Inicio" -Comment "ocultar el inicio de sesion de los usua
 Set-GPRegistryValue -Name "Ocultar Inicio" -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\policies\system" -ValueName dontdisplaylastusername -Type Dword -Value 1
 
 #Añadimos la directiva a una OU
-New-GPLink -Name "Ocultar Inicio" -Target "DC=Dominio,DC=com"
+New-GPLink -Name "Ocultar Inicio" -Target "DC=$($dom.Split(".")[0]),DC=$($dom.Split(".")[1])"
 
 #forzamos su inicio
 gpupdate /force
