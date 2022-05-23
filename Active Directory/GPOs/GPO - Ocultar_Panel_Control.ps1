bc7cd8197bd226a8ebb59a816f70ab1a31b33b87
Conde <79221088+conde26@users.noreply.github.com>
2022-05-23 14:02:04 +0200

    Update GPO - Ocultar_Panel_Control.ps1

--- Active Directory/GPOs/GPO - Ocultar_Panel_Control.ps1
+++ Active Directory/GPOs/GPO - Ocultar_Panel_Control.ps1
@@ -1,6 +1,9 @@
 ﻿#Ocultar panel de control
 #Author: Jose Conde 
 
+#Input domain
+Write-Host -NoNewline -ForegroundColor Yellow "[!] Indica el nombre de tu dominio: "; $dom=Read-Host 
+
 #Creamos GPO con un nombre y comentario
 New-GPO -Name "PanelControl" -Comment "ocultar el inicio de sesion de los usuarios"
 
@@ -8,8 +11,7 @@ New-GPO -Name "PanelControl" -Comment "ocultar el inicio de sesion de los usuari
 Set-GPRegistryValue -Name "PanelControl" -Key "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -ValueName NoControlPanel -Type DWORD -Value 1
 
 #Añadimos la directiva
-New-GPLink -Name "PanelControl" -Target "DC=Dominio,DC=com"
+New-GPLink -Name "PanelControl" -Target "DC=$($dom.Split(".")[0]),DC=$($dom.Split(".")[1])"
 
 #Forzamos actualización
 gpupdate /force 
-
